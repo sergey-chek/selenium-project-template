344cd8e12d6a3c3d6abb600aaec9cea6ed13c6c4
Sergey Chekurin <schek.it@gmail.com>
2021-01-08 23:46:48 +0300

    test_feature_1.py is created

---
+++ tests/conftest.py
@@ -0,0 +1,26 @@
+"""
+Shared fixtures for project.
+"""
+
+
+import pytest
+import selenium.webdriver
+
+
+@pytest.fixture
+def driver():
+    """Initializing the WebDriver instance and tearing it down after use."""
+
+    # Initialize a WebDriver instance
+    driver = selenium.webdriver.Chrome()
+    driver.implicitly_wait(10)
+    yield driver
+
+    # Quit the WebDriver instance
+    driver.quit()
+
+
+
+
+
+
---
+++ tests/test_feature_1.py
@@ -0,0 +1,25 @@
+"""
+These tests cover Feature #1. Searching on google.com
+    Any additional information about functionality...
+"""
+
+def test_feature_1_basic_search(driver):
+    """
+    Given the Google page is displayed
+    When the user searches for "python"
+        Then the search result title contains "python"
+            And the search result query is "python"
+            And the search result links in lower case contain "python"
+    """
+
+    # Given the Google page is displayed
+    # When the user searches for "python"
+    # Then the search result title contains "python"
+    # And the search result query is "python"
+    # And the search result links in lower case contain "python"
+    raise NotImplementedError
+
+
+
+
+

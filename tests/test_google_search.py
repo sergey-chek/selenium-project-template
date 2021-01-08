68ac179a58d71615c613f9fa144029367681d78c
Sergey Chekurin <schek.it@gmail.com>
2021-01-09 00:10:15 +0300

    test_basic_search, test_search_on_result_page are added

---
+++ tests/test_google_search.py
@@ -0,0 +1,64 @@
+"""
+These tests cover Feature #1. Searching on google.com
+    Any additional information about functionality...
+"""
+
+def test_basic_search(driver):
+    """
+    Given the Google page is displayed
+    When the user searches for "python"
+        Then the search result title contains "python"
+            And the search result query is "python"
+            And the search result links in lower case contain "python"
+    """
+
+    # Given the Google page is displayed
+    # TODO
+
+    # When the user searches for "python"
+    # TODO
+
+    # Then the search result title contains "python"
+    # TODO
+
+    # And the search result query is "python"
+    # TODO
+
+    # And the search result links in lower case contain "python"
+    # TODO
+
+    raise NotImplementedError
+
+
+def test_search_on_result_page(driver):
+    """
+    Given the Google page is displayed
+    Given the user searches for "python" and presses Enter
+    Given the search result page is displayed
+    When the user searches for "java" on the search result page
+        Then the search result title contains "java"
+            And the search result query is "java"
+            And the search result links in lower case contain "java"
+    """
+
+    # Given the Google page is displayed
+    # Given the user searches for "python" and presses Enter
+    # Given the search result page is displayed
+    # TODO
+
+    # When the user searches for "java" on the search result page
+    # TODO
+
+    # Then the search result title contains "java"
+    # TODO
+
+    # And the search result query is "java"
+    # TODO
+
+    # And the search result links in lower case contain "java"
+    # TODO
+
+    raise NotImplementedError
+
+
+

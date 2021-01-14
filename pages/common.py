93badca9046c2cd027e94c35f9cab723b2d54f78
Sergey Chekurin <schek.it@gmail.com>
2021-01-15 01:07:24 +0300

    Module with common functions was added

--- pages/common.py
+++ pages/common.py
@@ -4,7 +4,7 @@ Common utils for tests in project
 import datetime
 
 
-def generate_screenshot_name(test_name: str, file_extension: str) -> str:
+def generate_screenshot_file_name(test_name: str, file_extension: str) -> str:
     """
     This function generates file name for a screenshot.
     Format: "YYYY-MM-DD-HH-MM-SS-MMMMMM-test_name.file_extention".
@@ -12,3 +12,13 @@ def generate_screenshot_name(test_name: str, file_extension: str) -> str:
     """
     t = datetime.datetime.now()
     return f"{t.year}-{t.month}-{t.day}-{t.hour}-{t.second}-{t.minute}-{t.microsecond}-{test_name}.{file_extension}"
+
+
+def generate_screenshot_folder_name(test_name: str, file_extension: str) -> dict[str, str]:
+    """
+    This function generates folder name for a screenshot.
+    Format: "YYYY-MM-DD".
+    For example, "2021-01-15".
+    """
+    t = datetime.datetime.now()
+    return f"{t.year}-{t.month}-{t.day}"
